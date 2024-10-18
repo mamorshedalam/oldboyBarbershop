faed963ee5db8b40e3e7e9dd23c3f1bd81769c82
mamorshedalam <ma.morshedalampro@gmail.com>
2024-10-18 20:38:43 +0800

    project init

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  content: ["*.{html,js}"],
+  theme: {
+    extend: {},
+    fontFamily: {
+      'sans': ['Open-sans', 'ui-sans-serif', 'system-ui'],
+      'serif': ['ui-serif', 'Georgia'],
+      'mono': ['ui-monospace', 'SFMono-Regular'],
+      'rex': ['rex', 'ui-sans-serif', 'system-ui'],
+      'rex-inline': ['rex-inline', 'ui-sans-serif', 'system-ui'],
+    }
+  },
+  plugins: [],
+};
