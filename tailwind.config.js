f367e0a1a437453004d06b1bb06842a67282a15f
mamorshedalam <ma.morshedalampro@gmail.com>
2024-11-04 18:50:11 +0800

    header added

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,6 +8,14 @@ module.exports = {
       'mono': ['ui-monospace', 'SFMono-Regular'],
       'rex': ['rex', 'ui-sans-serif', 'system-ui'],
       'rex-inline': ['rex-inline', 'ui-sans-serif', 'system-ui'],
+    },
+    screens:{
+      xs: "360px",
+      sm: "560px",
+      md: "700px",
+      lg: "1000px",
+      xl: "1200px",
+      '2xl': "1500px",
     }
   },
   plugins: [],
